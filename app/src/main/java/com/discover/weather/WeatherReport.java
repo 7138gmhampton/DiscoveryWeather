4e25414b9dbeacb325fe6730d3415abb203f18bc
7138gmhampton <0407435@abertay.ac.uk>
2020-01-28 16:38:28 +0000

    Create layout rough draft for report submission page (not including location stamp)

---
+++ app/src/main/java/com/discover/weather/WeatherReport.java
@@ -0,0 +1,14 @@
+package com.discover.weather;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class WeatherReport extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_weather_report);
+    }
+}
