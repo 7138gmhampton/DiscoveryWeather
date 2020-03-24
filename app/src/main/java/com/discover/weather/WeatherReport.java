ab3087065c295e560e042b99bfe77b3c62d2392f
7138gmhampton <0407435@abertay.ac.uk>
2020-03-24 16:15:33 +0000

    Create method to update the displayed number for the wind direction

--- app/src/main/java/com/discover/weather/WeatherReport.java
+++ app/src/main/java/com/discover/weather/WeatherReport.java
@@ -3,12 +3,36 @@ package com.discover.weather;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.SeekBar;
+import android.widget.TextView;
 
-public class WeatherReport extends AppCompatActivity {
+public class WeatherReport extends AppCompatActivity
+{
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_weather_report);
+
+        //updateWindDirectionDisplay();
+    }
+
+    @Override
+    protected void onStart()
+    {
+        super.onStart();
+
+        updateWindDirectionDisplay();
+    }
+
+    private void updateWindDirectionDisplay()
+    {
+        SeekBar directionSetter = findViewById(R.id.seekWind);
+        int currentDirection = directionSetter.getProgress();
+        TextView directionDisplay = findViewById(R.id.textDirectionDisplay);
+
+        directionDisplay.setText(Integer.toString(currentDirection));
     }
 }
