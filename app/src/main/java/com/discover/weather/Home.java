4fc66619dbb3ebd547a294ff7b6e9bac51d7c184
7138gmhampton <0407435@abertay.ac.uk>
2020-03-24 15:57:05 +0000

    Implement change of activity for clicking the post button

--- app/src/main/java/com/discover/weather/Home.java
+++ app/src/main/java/com/discover/weather/Home.java
@@ -2,13 +2,34 @@ package com.discover.weather;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 
-public class Home extends AppCompatActivity {
-
+public class Home extends AppCompatActivity implements OnClickListener
+{
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        Button btnPost = findViewById(R.id.buttonPost);
+        btnPost.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view)
+    {
+        switch (view.getId()) {
+            case R.id.buttonPost:
+                Intent changeActivity = new Intent(
+                        getApplicationContext(),
+                        WeatherReport.class);
+                startActivity(changeActivity);
+                break;
+        }
     }
 }
