c3d5104162d7e50db8b1e2f9e746f1e29d7a2a33
7138gmhampton <0407435@abertay.ac.uk>
2020-03-24 18:59:41 +0000

    Remove unneeded import

--- app/src/main/java/com/discover/weather/Home.java
+++ app/src/main/java/com/discover/weather/Home.java
@@ -1,12 +1,11 @@
 package com.discover.weather;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.view.View.OnClickListener;
-import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class Home extends AppCompatActivity implements OnClickListener
 {
