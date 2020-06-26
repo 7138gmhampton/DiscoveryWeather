dd5b794171849a87ce8e1d70e15987ee180484da
7138gmhampton <0407435@abertay.ac.uk>
2020-06-26 17:39:36 +0100

    Remove corpse code

--- app/src/main/java/com/discover/weather/ConditionFragment.java
+++ app/src/main/java/com/discover/weather/ConditionFragment.java
@@ -22,55 +22,17 @@ import com.discover.weather.dummy.DummyContent.DummyItem;
 import java.util.List;
 import java.util.concurrent.locks.Condition;
 
-/**
- * A fragment representing a list of Items.
- * <p/>
- * Activities containing this fragment MUST implement the
- * interface.
- */
-public class ConditionFragment extends DialogFragment {
-
-    // TODO: Customize parameter argument names
-//    private static final String ARG_COLUMN_COUNT = "column-count";
-//    // TODO: Customize parameters
-//    private int mColumnCount = 1;
-//    private OnListFragmentInteractionListener mListener;
-
-    /**
-     * Mandatory empty constructor for the fragment manager to instantiate the
-     * fragment (e.g. upon screen orientation changes).
-     */
-
+public class ConditionFragment extends DialogFragment
+{
     ConditionFragmentListener listener;
 
-    public ConditionFragment() {
-    }
-
-    // TODO: Customize parameter initialization
-    @SuppressWarnings("unused")
-//    public static ConditionFragment newInstance(int columnCount) {
-//        ConditionFragment fragment = new ConditionFragment();
-//        Bundle args = new Bundle();
-//        args.putInt(ARG_COLUMN_COUNT, columnCount);
-//        fragment.setArguments(args);
-//        return fragment;
-//    }
+    public ConditionFragment() { }
 
     @Override
     public Dialog onCreateDialog(Bundle savedInstanceState)
     {
         AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
 
-//        builder.setMessage(R.string.display_error).
-//                setPositiveButton(R.string.display_error,
-//                        new DialogInterface.OnClickListener()
-//                        {
-//                            @Override
-//                            public void onClick(DialogInterface dialog, int which)
-//                            {
-//                                //This is a dummy anyway
-//                            }
-//                        });
         builder.setTitle(R.string.label_conditions).
                 setItems(R.array.dummy_conditions, new DialogInterface.OnClickListener()
                 {
@@ -98,67 +60,6 @@ public class ConditionFragment extends DialogFragment {
                     " must implement ConditionFragmentListener");
         }
     }
-
-//    @Override
-//    public void onCreate(Bundle savedInstanceState) {
-//        super.onCreate(savedInstanceState);
-//
-//        if (getArguments() != null) {
-//            mColumnCount = getArguments().getInt(ARG_COLUMN_COUNT);
-//        }
-//    }
-
-//    @Override
-//    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-//                             Bundle savedInstanceState) {
-//        View view = inflater.inflate(R.layout.fragment_condition_list, container, false);
-//
-//        // Set the adapter
-//        if (view instanceof RecyclerView) {
-//            Context context = view.getContext();
-//            RecyclerView recyclerView = (RecyclerView) view;
-//            if (mColumnCount <= 1) {
-//                recyclerView.setLayoutManager(new LinearLayoutManager(context));
-//            } else {
-//                recyclerView.setLayoutManager(new GridLayoutManager(context, mColumnCount));
-//            }
-//            recyclerView.setAdapter(new MyConditionRecyclerViewAdapter(DummyContent.ITEMS, mListener));
-//        }
-//        return view;
-//    }
-
-
-//    @Override
-//    public void onAttach(Context context) {
-//        super.onAttach(context);
-//        if (context instanceof OnListFragmentInteractionListener) {
-//            mListener = (OnListFragmentInteractionListener) context;
-//        } else {
-//            throw new RuntimeException(context.toString()
-//                    + " must implement OnListFragmentInteractionListener");
-//        }
-//    }
-
-//    @Override
-//    public void onDetach() {
-//        super.onDetach();
-//        mListener = null;
-//    }
-
-    /**
-     * This interface must be implemented by activities that contain this
-     * fragment to allow an interaction in this fragment to be communicated
-     * to the activity and potentially other fragments contained in that
-     * activity.
-     * <p/>
-     * See the Android Training lesson <a href=
-     * "http://developer.android.com/training/basics/fragments/communicating.html"
-     * >Communicating with Other Fragments</a> for more information.
-     */
-//    public interface OnListFragmentInteractionListener {
-//        // TODO: Update argument type and name
-//        void onListFragmentInteraction(DummyItem item);
-//    }
 
     public interface ConditionFragmentListener
     {
