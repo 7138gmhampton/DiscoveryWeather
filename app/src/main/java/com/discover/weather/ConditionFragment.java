7ec20ea39a44d4fb114c50b55ff2b71cb7011314
7138gmhampton <0407435@abertay.ac.uk>
2020-06-30 16:13:03 +0100

    Replace fixed input of items with adapter

--- app/src/main/java/com/discover/weather/ConditionFragment.java
+++ app/src/main/java/com/discover/weather/ConditionFragment.java
@@ -15,6 +15,7 @@ import androidx.recyclerview.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
 
 import com.discover.weather.dummy.DummyContent;
 import com.discover.weather.dummy.DummyContent.DummyItem;
@@ -32,16 +33,26 @@ public class ConditionFragment extends DialogFragment
     public Dialog onCreateDialog(Bundle savedInstanceState)
     {
         AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+//        String conditions[] =
+        ArrayAdapter conditions = ArrayAdapter.createFromResource(getContext(), R.array.dummy_conditions, android.R.layout.simple_list_item_1);
 
         builder.setTitle(R.string.label_conditions).
-                setItems(R.array.dummy_conditions, new DialogInterface.OnClickListener()
+//                setItems(R.array.dummy_conditions, new DialogInterface.OnClickListener()
+//                {
+//                    @Override
+//                    public void onClick(DialogInterface dialog, int which)
+//                    {
+//                        listener.onClickCondition(ConditionFragment.this, which);
+//                    }
+            setAdapter(conditions, new DialogInterface.OnClickListener()
+            {
+                @Override
+                public void onClick(DialogInterface dialog, int which)
                 {
-                    @Override
-                    public void onClick(DialogInterface dialog, int which)
-                    {
-                        listener.onClickCondition(ConditionFragment.this, which);
-                    }
-                });
+                    listener.onClickCondition(ConditionFragment.this, which);
+                }
+
+            });
 
         return builder.create();
     }
