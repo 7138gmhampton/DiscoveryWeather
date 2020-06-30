adbd70070df4a33698eda32c74bb8c7abd9c1403
7138gmhampton <0407435@abertay.ac.uk>
2020-06-30 18:53:55 +0100

    Remove unneeded imports

--- app/src/main/java/com/discover/weather/ConditionFragment.java
+++ app/src/main/java/com/discover/weather/ConditionFragment.java
@@ -5,33 +5,17 @@ import android.app.Dialog;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.ArrayAdapter;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.DialogFragment;
-import androidx.fragment.app.Fragment;
-import androidx.recyclerview.widget.GridLayoutManager;
-import androidx.recyclerview.widget.LinearLayoutManager;
-import androidx.recyclerview.widget.RecyclerView;
 
-import android.util.Log;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.ArrayAdapter;
-
-//import com.discover.weather.dummy.DummyContent;
-//import com.discover.weather.dummy.DummyContent.DummyItem;
 import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
-import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.QueryDocumentSnapshot;
 import com.google.firebase.firestore.QuerySnapshot;
-import com.google.firebase.firestore.DocumentReference;
-
-import java.util.List;
-import java.util.concurrent.locks.Condition;
 
 public class ConditionFragment extends DialogFragment
 {
