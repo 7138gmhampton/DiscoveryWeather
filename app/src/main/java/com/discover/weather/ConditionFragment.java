561798479b95bf8e555322c24e85ffcfea03f752
7138gmhampton <0407435@abertay.ac.uk>
2020-06-30 18:50:24 +0100

    Deploy prototype query of conditions

--- app/src/main/java/com/discover/weather/ConditionFragment.java
+++ app/src/main/java/com/discover/weather/ConditionFragment.java
@@ -6,12 +6,14 @@ import android.content.Context;
 import android.content.DialogInterface;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.DialogFragment;
 import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -19,6 +21,14 @@ import android.widget.ArrayAdapter;
 
 import com.discover.weather.dummy.DummyContent;
 import com.discover.weather.dummy.DummyContent.DummyItem;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
+import com.google.firebase.firestore.DocumentReference;
 
 import java.util.List;
 import java.util.concurrent.locks.Condition;
@@ -36,6 +46,20 @@ public class ConditionFragment extends DialogFragment
 //        String conditions[] =
         ArrayAdapter conditions = ArrayAdapter.createFromResource(getContext(), R.array.dummy_conditions, android.R.layout.simple_list_item_1);
 
+        FirebaseFirestore database = FirebaseFirestore.getInstance();
+        database.collection("condition").get().addOnCompleteListener(
+            new OnCompleteListener<QuerySnapshot>()
+            {
+                public void onComplete(@NonNull Task<QuerySnapshot> task)
+                {
+                    if (task.isSuccessful())
+                        for (QueryDocumentSnapshot document : task.getResult())
+                            Log.d("database", document.getId() + ": " +
+                                    document.getString("display"));
+                    else Log.d("database", "Error querying: " +
+                            task.getException());
+                }
+            });
         builder.setTitle(R.string.label_conditions).
 //                setItems(R.array.dummy_conditions, new DialogInterface.OnClickListener()
 //                {
