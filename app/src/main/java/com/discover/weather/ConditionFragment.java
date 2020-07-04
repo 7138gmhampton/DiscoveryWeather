fa3c854c74c622216b5f3d4ce2e0802d8eb698f0
7138gmhampton <0407435@abertay.ac.uk>
2020-07-04 19:25:45 +0100

    Remove corpse/dead code

--- app/src/main/java/com/discover/weather/ConditionFragment.java
+++ app/src/main/java/com/discover/weather/ConditionFragment.java
@@ -5,26 +5,16 @@ import android.app.Dialog;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.os.Bundle;
-import android.util.Log;
 import android.widget.ArrayAdapter;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.DialogFragment;
 
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.Task;
-import com.google.firebase.firestore.FirebaseFirestore;
-import com.google.firebase.firestore.QueryDocumentSnapshot;
-import com.google.firebase.firestore.QuerySnapshot;
-
 import java.util.HashMap;
-import java.util.Map;
-import java.util.Objects;
 
 public class ConditionFragment extends DialogFragment
 {
     private ConditionFragmentListener listener;
-//    private Map<Integer, String> overall_condition_options_;
 
     public ConditionFragment() { }
 
@@ -32,16 +22,7 @@ public class ConditionFragment extends DialogFragment
     @NonNull
     public Dialog onCreateDialog(Bundle savedInstanceState)
     {
-//        Bundle condition_options_argument = getArguments();
-//        HashMap<Integer,String> temp_map = (HashMap<Integer, String>) condition_options_argument.getSerializable("options");
-//        overall_condition_options_ = new HashMap<Integer,String>();
-//        overall_condition_options_.putAll(temp_map);
         AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
-//        String[] condition_options = overall_condition_options_.values().toArray(new String[overall_condition_options_.size()]);
-//        ArrayAdapter conditions = new ArrayAdapter(
-//                Objects.requireNonNull(getContext()),
-//                android.R.layout.simple_list_item_1);
-//        conditions.addAll(condition_options);
 
         builder.setTitle(R.string.label_conditions).
             setAdapter(prepareAdapter(
@@ -77,32 +58,6 @@ public class ConditionFragment extends DialogFragment
     {
         void onClickCondition(DialogFragment dialog, int index);
     }
-
-    private void pollOverallConditionsFromDatabase()
-    {
-        FirebaseFirestore database = FirebaseFirestore.getInstance();
-        database.collection("condition").get().addOnCompleteListener(
-            new OnCompleteListener<QuerySnapshot>()
-            {
-                public void onComplete(@NonNull Task<QuerySnapshot> task)
-                {
-                    if (task.isSuccessful())
-                        //noinspection ConstantConditions
-                        for (QueryDocumentSnapshot document : task.getResult())
-                            Log.d("database", document.getId() + ": " +
-                                    document.getString("display"));
-                    else Log.d("database", "Error querying: " +
-                            task.getException());
-                }
-            });
-    }
-
-//    private void logConditionOptions()
-//    {
-//        Log.d("pass", "Condition set size: " + overall_condition_options_.size());
-//        for (Map.Entry<Integer, String> condition : overall_condition_options_.entrySet())
-//            Log.d("pass",condition.getKey().toString() + " -> " + condition.getValue());
-//    }
 
     private ArrayAdapter prepareAdapter(HashMap<Integer,String> conditions)
     {
