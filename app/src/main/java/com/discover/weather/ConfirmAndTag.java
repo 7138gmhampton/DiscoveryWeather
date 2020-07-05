11b70835fef9ff393ca2cfb8a4d42451c1c01a8d
7138gmhampton <0407435@abertay.ac.uk>
2020-07-05 13:57:38 +0100

    Create reading confirmation activity

---
+++ app/src/main/java/com/discover/weather/ConfirmAndTag.java
@@ -0,0 +1,16 @@
+package com.discover.weather;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ConfirmAndTag extends AppCompatActivity
+{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_confirm_and_tag);
+    }
+}
