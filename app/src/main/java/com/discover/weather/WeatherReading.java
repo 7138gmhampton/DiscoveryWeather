dba0a98d005ef846ed3590acf1fb6b592cd8b412
7138gmhampton <0407435@abertay.ac.uk>
2020-07-05 15:48:24 +0100

    Implement skeleton of class methods and fields

--- app/src/main/java/com/discover/weather/WeatherReading.java
+++ app/src/main/java/com/discover/weather/WeatherReading.java
@@ -2,4 +2,12 @@ package com.discover.weather;
 
 public class WeatherReading
 {
+    private float temperature_in_celsius_;
+
+    public WeatherReading(float temperature_in_celsius)
+    {
+        temperature_in_celsius_ = temperature_in_celsius;
+    }
+
+    public float getTemperature() { return temperature_in_celsius_; }
 }
