6b79ab975d94ce3e9f76d7873569ace7ea9021c6
7138gmhampton <0407435@abertay.ac.uk>
2020-07-05 15:54:15 +0100

    Add parcel constructor overload

--- app/src/main/java/com/discover/weather/WeatherReading.java
+++ app/src/main/java/com/discover/weather/WeatherReading.java
@@ -1,13 +1,26 @@
 package com.discover.weather;
 
+import android.os.Parcel;
+
 public class WeatherReading
 {
-    private float temperature_in_celsius_;
+    private float temperature_in_celsius;
+    private static final int NO_DATA_POINTS = 1;
 
     public WeatherReading(float temperature_in_celsius)
     {
-        temperature_in_celsius_ = temperature_in_celsius;
+        this.temperature_in_celsius = temperature_in_celsius;
     }
 
-    public float getTemperature() { return temperature_in_celsius_; }
+    public WeatherReading(Parcel in)
+    {
+        String[] data = new String[NO_DATA_POINTS];
+        in.readStringArray(data);
+
+        this.temperature_in_celsius = Float.parseFloat(data[0]);
+    }
+
+    public float getTemperature() { return temperature_in_celsius; }
+
+
 }
