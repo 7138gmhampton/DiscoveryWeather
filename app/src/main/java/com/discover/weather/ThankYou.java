693c8ad93ff517a2584ee1fe8fea8311c5ad3432
7138gmhampton <0407435@abertay.ac.uk>
2020-07-09 17:34:56 +0100

    Create ticker that counts down time to display the thank you message

--- app/src/main/java/com/discover/weather/ThankYou.java
+++ app/src/main/java/com/discover/weather/ThankYou.java
@@ -3,13 +3,31 @@ package com.discover.weather;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.os.Handler;
+import android.util.Log;
 
 public class ThankYou extends AppCompatActivity
 {
+    private int time_to_display;
+
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_thank_you);
+
+        time_to_display = 5;
+
+        final Handler ticker = new Handler();
+        ticker.postDelayed(new Runnable()
+        {
+            @Override
+            public void run()
+            {
+                --time_to_display;
+                Log.d("ui-behave", "Display remain " + time_to_display + "s");
+                ticker.postDelayed(this, 1000);
+            }
+        }, 1000);
     }
 }
