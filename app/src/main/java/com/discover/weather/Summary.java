67d3f58732a7d21a406d272244967a535c4b1d70
7138gmhampton <0407435@abertay.ac.uk>
2020-07-12 23:22:24 +0100

    Remove corpse code

--- app/src/main/java/com/discover/weather/Summary.java
+++ app/src/main/java/com/discover/weather/Summary.java
@@ -14,24 +14,7 @@ class Summary
     static String author(Context context, WeatherReading reading)
     {
         Resources resources = context.getResources();
-//        String summary = intro;
-//
-//        summary += ConditionOptions.getInstance().getOptions().get(reading.getConditionCode()) +
-//            System.lineSeparator();
-//        summary += appendDetail("Temperature: ", reading.getTemperature(),
-//            resources.getString(R.string.label_temp_units));
-//        summary += appendDetail("Pressure: ", reading.getPressure(),
-//            resources.getString(R.string.label_pressure_unit));
-//        summary += appendDetail("Wind Speed: ", reading.getWindSpeed(),
-//            resources.getString(R.string.label_speed_unit));
-//        summary += appendDetail("Wind Direction: ", reading.getWindDirection(),
-//            resources.getString(R.string.units_direction));
-//        summary += appendDetail("Rainfall: ", reading.getRainfall(),
-//            resources.getString(R.string.label_precipitation_unit));
-//        summary += appendDetail("Snowfall: ", reading.getSnowfall(),
-//            resources.getString(R.string.label_precipitation_unit));
-//
-//        summary += outro;
+
         return intro +
             ConditionOptions.getInstance().getOptions().get(reading.getConditionCode()) +
             System.lineSeparator() +
@@ -52,16 +35,6 @@ class Summary
 
     private static String appendDetail(String name, Float metric, String unit)
     {
-//        String detail = "";
-//
-//        if (metric != null) {
-//            detail += name;
-//            detail += String.format("%.2f", metric);
-//            detail += unit;
-//            detail += System.lineSeparator();
-//        }
-//
-//        return detail;
         if (metric != null)
             return name + String.format(Locale.UK,"%.2f", metric) + unit +
                 System.lineSeparator();
@@ -70,16 +43,6 @@ class Summary
 
     private static String appendDetail(String name, Integer metric, String unit)
     {
-//        String detail = "";
-//
-//        if (metric != null) {
-//            detail += name;
-//            detail += metric.toString();
-//            detail += unit;
-//            detail += System.lineSeparator();
-//        }
-//
-//        return detail;
         if (metric != null)
             return  name + metric.toString() + unit + System.lineSeparator();
         return "";
