199d288249409e34db6bfdffd6209dfcd9286ff3
7138gmhampton <0407435@abertay.ac.uk>
2020-07-12 23:12:37 +0100

    Shorten method by inlining return

--- app/src/main/java/com/discover/weather/Summary.java
+++ app/src/main/java/com/discover/weather/Summary.java
@@ -14,25 +14,40 @@ class Summary
     static String author(Context context, WeatherReading reading)
     {
         Resources resources = context.getResources();
-        String summary = intro;
-
-        summary += ConditionOptions.getInstance().getOptions().get(reading.getConditionCode()) +
-            System.lineSeparator();
-        summary += appendDetail("Temperature: ", reading.getTemperature(),
-            resources.getString(R.string.label_temp_units));
-        summary += appendDetail("Pressure: ", reading.getPressure(),
-            resources.getString(R.string.label_pressure_unit));
-        summary += appendDetail("Wind Speed: ", reading.getWindSpeed(),
-            resources.getString(R.string.label_speed_unit));
-        summary += appendDetail("Wind Direction: ", reading.getWindDirection(),
-            resources.getString(R.string.units_direction));
-        summary += appendDetail("Rainfall: ", reading.getRainfall(),
-            resources.getString(R.string.label_precipitation_unit));
-        summary += appendDetail("Snowfall: ", reading.getSnowfall(),
-            resources.getString(R.string.label_precipitation_unit));
-
-        summary += outro;
-        return summary;
+//        String summary = intro;
+//
+//        summary += ConditionOptions.getInstance().getOptions().get(reading.getConditionCode()) +
+//            System.lineSeparator();
+//        summary += appendDetail("Temperature: ", reading.getTemperature(),
+//            resources.getString(R.string.label_temp_units));
+//        summary += appendDetail("Pressure: ", reading.getPressure(),
+//            resources.getString(R.string.label_pressure_unit));
+//        summary += appendDetail("Wind Speed: ", reading.getWindSpeed(),
+//            resources.getString(R.string.label_speed_unit));
+//        summary += appendDetail("Wind Direction: ", reading.getWindDirection(),
+//            resources.getString(R.string.units_direction));
+//        summary += appendDetail("Rainfall: ", reading.getRainfall(),
+//            resources.getString(R.string.label_precipitation_unit));
+//        summary += appendDetail("Snowfall: ", reading.getSnowfall(),
+//            resources.getString(R.string.label_precipitation_unit));
+//
+//        summary += outro;
+        return intro +
+            ConditionOptions.getInstance().getOptions().get(reading.getConditionCode()) +
+            System.lineSeparator() +
+            appendDetail("Temperature: ", reading.getTemperature(),
+                resources.getString(R.string.label_temp_units)) +
+            appendDetail("Pressure: ", reading.getPressure(),
+                resources.getString(R.string.label_pressure_unit)) +
+            appendDetail("Wind Speed: ", reading.getWindSpeed(),
+                resources.getString(R.string.label_speed_unit)) +
+            appendDetail("Wind Direction: ", reading.getWindDirection(),
+                resources.getString(R.string.units_direction)) +
+            appendDetail("Rainfall: ", reading.getRainfall(),
+                resources.getString(R.string.label_precipitation_unit)) +
+            appendDetail("Snowfall: ", reading.getSnowfall(),
+                resources.getString(R.string.label_precipitation_unit)) +
+            outro;
     }
 
     private static String appendDetail(String name, Float metric, String unit)
