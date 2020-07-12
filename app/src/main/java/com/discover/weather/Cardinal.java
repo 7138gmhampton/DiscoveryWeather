fea02d5bcf187e215893c47ec6c127b4d6da525f
7138gmhampton <0407435@abertay.ac.uk>
2020-07-12 23:53:51 +0100

    Simplify methods through inlining returns

--- app/src/main/java/com/discover/weather/Cardinal.java
+++ app/src/main/java/com/discover/weather/Cardinal.java
@@ -1,46 +1,37 @@
 package com.discover.weather;
 
 import java.util.HashMap;
-import java.util.Map;
 import java.util.TreeMap;
 
 public class Cardinal
 {
-    private static final TreeMap<Integer,String> direction_ranges = new TreeMap<>();
+    private static final TreeMap<Integer,String> RANGES = new TreeMap<>();
     static {
-        direction_ranges.put(0,"N");
-        direction_ranges.put(22,"NE");
-        direction_ranges.put(67,"E");
-        direction_ranges.put(113,"SE");
-        direction_ranges.put(158,"S");
-        direction_ranges.put(202,"SW");
-        direction_ranges.put(247,"W");
-        direction_ranges.put(293,"NW");
-        direction_ranges.put(338,"N");
+        RANGES.put(0,"N");
+        RANGES.put(22,"NE");
+        RANGES.put(67,"E");
+        RANGES.put(113,"SE");
+        RANGES.put(158,"S");
+        RANGES.put(202,"SW");
+        RANGES.put(247,"W");
+        RANGES.put(293,"NW");
+        RANGES.put(338,"N");
     }
-    private static final HashMap<String,Integer> exact_directions = new HashMap<>();
+    private static final HashMap<String,Integer> EXACT_DIRECTIONS = new HashMap<>();
     static {
-        exact_directions.put("N", 0);
-        exact_directions.put("NE", 45);
-        exact_directions.put("E", 90);
-        exact_directions.put("SE", 135);
-        exact_directions.put("S", 180);
-        exact_directions.put("SW", 225);
-        exact_directions.put("W", 270);
-        exact_directions.put("NW", 315);
+        EXACT_DIRECTIONS.put("N", 0);
+        EXACT_DIRECTIONS.put("NE", 45);
+        EXACT_DIRECTIONS.put("E", 90);
+        EXACT_DIRECTIONS.put("SE", 135);
+        EXACT_DIRECTIONS.put("S", 180);
+        EXACT_DIRECTIONS.put("SW", 225);
+        EXACT_DIRECTIONS.put("W", 270);
+        EXACT_DIRECTIONS.put("NW", 315);
     }
 
     @SuppressWarnings("ConstantConditions")
-    static String getCardinal(Integer absolute)
-    {
-        Map.Entry<Integer,String> entry = direction_ranges.floorEntry(absolute);
-
-        return entry .getValue();
-    }
+    static String getCardinal(Integer absolute) { return RANGES.floorEntry(absolute).getValue(); }
 
     @SuppressWarnings("ConstantConditions")
-    static int getAbsoluteDirection(String cardinal)
-    {
-        return exact_directions.get(cardinal);
-    }
+    static int getAbsoluteDirection(String cardinal) { return EXACT_DIRECTIONS.get(cardinal); }
 }
